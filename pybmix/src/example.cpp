befe4c670ec331927fa28ff90d1c7f1e5076f338
mberaha <berahamario@gmail.com>
2020-12-17 15:14:24 +0100

    working with our library!

--- pybmix/src/example.cpp
+++ pybmix/src/example.cpp
@@ -1,12 +1,25 @@
 #include <pybind11/pybind11.h>
 
+#include <Eigen/Dense>
+
+#include "bayesmix/src/utils/distributions.hpp"
+#include "bayesmix/src/utils/rng.hpp"
+
 namespace py = pybind11;
 
 int add(int i, int j) { return i + j; }
 
 int subtract(int i, int j) { return i - j; }
 
+int draw_uniform(int start, int end) {
+  Eigen::VectorXd probas = Eigen::VectorXd::Ones(end - start);
+  probas /= probas.sum();
+  return bayesmix::categorical_rng(probas, bayesmix::Rng::Instance().get(),
+                                   start);
+}
+
 PYBIND11_MODULE(pybmix, m) {
   m.def("add", &add);
   m.def("subtract", &subtract);
+  m.def("draw_uniform", &draw_uniform);
 }
