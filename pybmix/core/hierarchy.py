f0409f404b9c5ab7d04284033a7f3cf1e735b90e
mberaha <berahamario@gmail.com>
2020-12-24 16:59:50 +0100

    skeleton of interface

---
+++ pybmix/core/hierarchy.py
@@ -0,0 +1,58 @@
+import abc
+import numpy as np
+
+import pybmix.proto.hierarchy_prior_pb2 as hprior
+
+
+class BaseHierarchy(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def make_default_fixed_params(y):
+        pass
+
+
+class UnivariateNormal(BaseHierarchy):
+    NAME = "NNIG"
+
+    def __init__(self, prior_params=None):
+        self.prior_params = hprior.NNIGPrior()
+        if isinstance(prior_params, hprior.NNIGPrior.FixedValues):
+            self.prior_params.fixed_values.CopyFrom(prior_params)
+        elif isinstance(prior_params, hprior.NNIGPrior.NormalMeanPrior):
+            self.prior_params.normal_mean_prior.CopyFrom(prior_params)
+        elif isinstance(prior_params, hprior.NNIGPrior.NGGPrior):
+            self.prior_params.ngg_prior.CopyFrom(prior_params)
+        else:
+            raise ValueError(
+                "expected 'prior_params' to be of instance "
+                "FixedValues, NormalMeanPrior or NGGPrior, "
+                "found {0} instead".format(type(prior_params)))
+         
+
+    def make_default_fixed_params(self, y, exp_num_clusters=5):
+        """ 
+        Follow the approach in [1] to define a weakly informative prior
+
+        Parameters
+        ----------
+        y : array_like of shape (n, ) 
+            The observed data
+        exp_num_clusters : int
+            An "a priori" guess of th number of clusters in the data
+
+        [1] Fraley, C., & Raftery, A. E. (2007). 
+            Bayesian regularization for normal mixture estimation and 
+            model-based clustering. Journal of classification, 24(2), 155-181.
+        """
+        self.prior_params.fixed_values.mean = np.mean(y)
+        self.prior_params.fixed_values.shape = 3
+        self.prior_params.fixed_values.scale = np.var(y) / exp_num_clusters
+        self.prior_params.var_scaling = 0.01
+
+
+class MultivariateNormal(BaseHierarchy):
+    NAME = "NNW"
+    pass
+
+
+class LinearModel(BaseHierarchy):
+    pass
--- pybmix/core/mixing.py
+++ pybmix/core/mixing.py
@@ -1,4 +1,5 @@
 import abc
+import logging
 import numpy as np
 
 from scipy.special import gamma
@@ -17,7 +18,7 @@ class BaseMixing(metaclass=abc.ABCMeta):
         """
         Evaluates the prior probability of the number of clusters on a
         grid
-        
+
         Parameters
         ----------
         grid : array-like of shape (n,)
@@ -29,24 +30,52 @@ class BaseMixing(metaclass=abc.ABCMeta):
 
 
 class DirichletProcessMixing(BaseMixing):
+    """ This class represents a Dirichlet Process used for mixing in a mixture
+    model. The Drichlet process depends on a 'total_mass' parameter, which
+    could also be random.
+
+    Parameters
+    ----------
+    total_mass : float greater than 0 or None
+                 total_mass (or concentration) parameter of the Dirichlet Process
+                 if None, assumes that 'total_mass_prior' is passed
+    total_mass_prior : pybmix.proto.distribution_pb2.GammaDistribution or None
+                       parameters for the prior distribution of 'total_mass'
+    """
+
+    NAME = "DP"
+
     def __init__(self, total_mass=None, total_mass_prior=None):
         self._check_args(total_mass, total_mass_prior)
-        self._build_prior_proto(total_mass, total_mass_prior)    
-        self.total_mass = total_mass
+        self._build_prior_proto(total_mass, total_mass_prior)
+        self.total_mass_fixed = total_mass
         self.total_mass_prior = total_mass_prior
 
     def prior_cluster_distribution(self, grid, nsamples):
+        """
+        Evaluates the prior probability of the number of clusters on a
+        grid
+
+        Parameters
+        ----------
+        grid : array-like of shape (n,)
+               Points where to evaluate the probability mass function
+        nsamples : int
+                   Number of samples
+        """
         if self.prior_proto.WhichOneof("totalmass") != "fixed_value":
-            raise NotImplementedError(
+            logging.warning(
                 "'prior_cluster_distribution' is implemented only for "
-                "fixed total_mass parameters")
+                "fixed total_mass parameter. Substituting the prior expected "
+                "value as fixed total_mass ")
+            total_mass = self.total_mass_prior.shape / self.total_mass_prior.rate
+        else:
+            total_mass = self.total_mass_fixed
 
         out = np.zeros_like(grid, dtype=np.float)
         for i, g in enumerate(grid):
-            out[i] = gamma(self.total_mass) / gamma(self.total_mass + nsamples) * \
-                 stirling(nsamples, g) * (self.total_mass) ** g
-            
-            print("val :", out[i])
+            out[i] = gamma(total_mass) / gamma(total_mass + nsamples) * \
+                stirling(nsamples, g) * (total_mass) ** g
 
         return out
 
@@ -65,7 +94,7 @@ class DirichletProcessMixing(BaseMixing):
 
         if total_mass is None and total_mass_prior is None:
             raise ValueError(error_msg)
-        
+
         if total_mass is not None and total_mass_prior is not None:
             raise ValueError(error_msg)
 
@@ -74,17 +103,22 @@ class DirichletProcessMixing(BaseMixing):
                 "Parameter 'total_mass' must be strictly greater than zero")
 
         if total_mass_prior is not None and not isinstance(
-            total_mass_prior, GammaDistribution):
+                total_mass_prior, GammaDistribution):
             error_msg = "parameter 'total_mass_prior' must be of instance " + \
                         "pybmix.proto.distribution_pb2.GammaDistribution"
             raise ValueError(error_msg)
 
-        if total_mass_prior is not None and  total_mass_prior.shape <= 0:
+        if total_mass_prior is not None and total_mass_prior.shape <= 0:
             error_msg = "parameter 'total_mass_prior.shape' must be strictly " + \
                         "greater than 0, found {0} instead".format(
                             total_mass_prior.shape)
-        
+
         if total_mass_prior is not None and total_mass_prior.rate <= 0:
             error_msg = "parameter 'total_mass_prior.rate' must be strictly " + \
                         "greater than 0, found {0} instead".format(
                             total_mass_prior.rate)
+
+
+class PitmanYorMixing(BaseMixing):
+    NAME = "PY"
+    pass
