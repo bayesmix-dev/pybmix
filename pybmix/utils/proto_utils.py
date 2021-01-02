3ea03689ce821594e2472edf99106d663f07803b
mberaha <berahamario@gmail.com>
2021-01-02 17:41:24 +0100

    more proto utils for extracting sub-chains

--- pybmix/utils/proto_utils.py
+++ pybmix/utils/proto_utils.py
@@ -2,7 +2,7 @@ import logging
 
 from google.protobuf.message import Message
 from google.protobuf.internal.containers import (
-    RepeatedCompositeFieldContainer,
+    MutableMapping, RepeatedCompositeFieldContainer,
     RepeatedScalarFieldContainer)
 from google.protobuf.pyext._message import RepeatedCompositeContainer
 
@@ -20,9 +20,9 @@ def set_oneof_field(fieldname, msg, val):
 
     Example
     -------
-    > protomsg = pybmix.proto.hierarchy_prior_pb2.NNIGPrior()
-    > prior = pybmix.proto.hierarchy_prior_pb2.NNIGPrior.FixedValues(mean=1)
-    > set_oneof_field("prior", protomsg, prior)
+    >>> protomsg = pybmix.proto.hierarchy_prior_pb2.NNIGPrior()
+    >>> prior = pybmix.proto.hierarchy_prior_pb2.NNIGPrior.FixedValues(mean=1)
+    >>> set_oneof_field("prior", protomsg, prior)
     """
     if fieldname not in msg.DESCRIPTOR.oneofs_by_name.keys():
         logging.error("fieldname {0} is not a 'oneof' field of {1}".format(
@@ -40,6 +40,53 @@ def set_oneof_field(fieldname, msg, val):
             continue
 
     return False
+
+
+def get_field(msg, field: str):
+    """Gets the field of an object, even if it is in a submessage
+
+    Parameters
+    ----------
+    msg: google.protobuf.message
+        a protobuf (object)
+    field: str
+        a field of this proto. If it is a nested field,
+        then we adopt the syntax a joining the subfields separated by '.'
+    """
+    subfields = field.split(".")
+    curr = msg
+    try:
+        for subfield in subfields:
+            index = -1
+            if subfield.endswith("]") and "[" in subfield[:-1]:
+                parts = subfield[:-1].split("[")
+                index = int(parts[1])
+                subfield = parts[0]
+            curr = _get_shallow_field(curr, subfield)
+            if index >= 0 and index < len(curr):
+                curr = curr[index]
+        return curr
+    except Exception as e:
+        return None
+
+
+def _get_shallow_field(msg, field: str):
+    """Internal method to get the value of the field of the protobuf.
+
+    This method doesn't go down the hierarchy and does not support the '.'
+    syntax. Use get_field instead
+    """
+    if isinstance(msg, MutableMapping):
+        try:
+            return msg[field]
+
+        except TypeError as e:
+            return msg[int(field)]
+
+    if hasattr(msg, field):
+        return getattr(msg, field)
+
+    return
 
 
 def get_oneof_names(oneof_name, msg):
