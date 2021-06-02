387865fec50628e83a9ff38882620ae63d0575c6
mberaha <berahamario@gmail.com>
2021-06-02 09:03:34 +0200

    readded autosummary + some test imports

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ release = '0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    # 'sphinx.ext.autosummary',
+    'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
