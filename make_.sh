2779d6a27f833512fd68151d6c31b314930a9c8c
EnricoPaglia <enrico.paglia@mail.polimi.it>
2022-06-16 22:37:44 +0200

    Removing examples, docs, tests and benchmarks folders

--- make_.sh
+++ make_.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd build/;
-cmake .. ;
+cmake .. -DDISABLE_DOCS=ON -DDISABLE_BENCHMARKS=ON -DDISABLE_TESTS=ON -DDISABLE_EXAMPLES=ON;
 make pybmixcpp;
 make generate_protos;
 make two_to_three;
