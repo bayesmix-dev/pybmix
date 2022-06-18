1075dc7f7a2f086a7aff74398d8fae306881bb2c
taguhiM <mesropyantag@gmail.com>
2022-06-18 16:26:51 +0200

    added argument build

--- make_.sh
+++ make_.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-cd build/;
-cmake .. -DDISABLE_DOCS=ON -DDISABLE_BENCHMARKS=ON -DDISABLE_TESTS=ON -DDISABLE_EXAMPLES=ON;
-make pybmixcpp;
-make generate_protos;
-make two_to_three;
+if [ -z "$1" ]; then
+  cd build/ || exit
+  cmake .. -DDISABLE_DOCS=ON -DDISABLE_BENCHMARKS=ON -DDISABLE_TESTS=ON -DDISABLE_EXAMPLES=ON
+  make pybmixcpp
+  make generate_protos
+  make two_to_three
+else
+  if [[ $1 == build ]]; then
+    rm -rf $1
+    mkdir $1
+    cd $1 || exit
+    cmake .. -DDISABLE_DOCS=ON -DDISABLE_BENCHMARKS=ON -DDISABLE_TESTS=ON -DDISABLE_EXAMPLES=ON
+    make pybmixcpp
+    make generate_protos
+    make two_to_three
+  else
+    echo "wrong argument, pass build"
+  fi
+fi
