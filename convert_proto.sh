dc0cc660c3ba70b7027f2f0cdb36b3dd95fba170
EnricoPaglia <enrico.paglia@mail.polimi.it>
2022-07-01 00:08:30 +0200

    change convert proto to conda prefix

--- convert_proto.sh
+++ convert_proto.sh
@@ -1,6 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
 
 # NEEDS TO BE SET MANUALLY
-ENV_DIR=/home/enrico98/miniconda3/envs/PYBMIX/
+ENV_DIR=$CONDA_PREFIX
 
 $ENV_DIR/bin/2to3 --output-dir=$SCRIPT_DIR/pybmix/proto -W -n $SCRIPT_DIR/pybmix/proto
