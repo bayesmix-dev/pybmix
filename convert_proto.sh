54ff21c6025f8bcb9578866c6f18c468f4f77935
taguhiM <mesropyantag@gmail.com>
2022-07-01 18:52:52 +0200

    small modifications

--- convert_proto.sh
+++ convert_proto.sh
@@ -1,6 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
 
 # NEEDS TO BE SET MANUALLY
-ENV_DIR=$CONDA_PREFIX
+TWO_TO_THREE_PATH=/home/taguhi/miniconda3/envs/pybmix_foo/bin/2to3
 
-$ENV_DIR/bin/2to3 --output-dir=$SCRIPT_DIR/pybmix/proto -W -n $SCRIPT_DIR/pybmix/proto
+$TWO_TO_THREE_PATH --output-dir=$SCRIPT_DIR/pybmix/proto -W -n $SCRIPT_DIR/pybmix/proto
