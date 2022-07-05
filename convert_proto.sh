a2b571a56864b4a0690edf28f82fac55b518f84d
EnricoPaglia <enrico.paglia@mail.polimi.it>
2022-07-05 17:30:14 +0200

    pwd in convert proto

--- convert_proto.sh
+++ convert_proto.sh
@@ -1,4 +1,4 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
+SCRIPT_DIR=$(pwd)/..
 
 # 2to3 must be in the path (e.g. export PYTHONPATH="path/to/2to3/")
 2to3 --output-dir=$SCRIPT_DIR/pybmix/proto -W -n $SCRIPT_DIR/pybmix/proto
